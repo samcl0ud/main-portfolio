27432509dfd871bf3b54449524b5d6024fb33eea
Sam King <sam.king.main@gmail.com>
2024-09-03 22:24:03 +0100

    new portfolio

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles/homepage.css";
+
+const Homepage = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <h2>Hey There.</h2>
+        <h2>I'm Sam.</h2>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Homepage;
